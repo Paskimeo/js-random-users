aefbd7bff147c61dcec51e1331c208eeeb9eed7b
paskimeo <paskimeo@gmail.com>
2022-08-03 17:42:18 +0200

    finito esercizio

--- axios.js
+++ axios.js
@@ -5,20 +5,63 @@ addBtn.addEventListener('click', addUser);
 
 
 // funzione addDog aggiunge un'immagine di cane alla gallery
-function addUser() {
+function addUser() 
+{
     console.log('aggiungo un user');
- // con axios chiamo l'api dei cani e prendo l'url dell'immagine
+ // con axios chiamo l'api e prendo i dati richiesti
  axios.get('https://randomuser.me/api/')
  
  //qui va la resposne con handle success
+ .then(function(response)
+    {
+        console.log(response);
+        const userImg = response.data.results[0].picture.medium;
+        console.log(userImg);
+        const userName = response.data.results[0].name.first;
+        console.log(userName);
+        const userSurname = response.data.results[0].name.last;
+        console.log(userSurname);
 
+
+        createUser(userImg, userName, userSurname);
+    })
 
 
  //qui va handle error
  .catch(function (error) {
     // handle error
-    console.log(  error, "Uh oh, something has gone wrong. Please tweet us @randomapi about the issue. Thank you."
-    );
+    console.log( error)
   });
  
+
+  function createUser(url, name, surname){
+        
+    //creo il tag immagine da mettere nel template
+    let image = document.createElement('img');
+    image.src = url;
+    image.className = 'img-fluid p-2';
+
+    //inserisco nome e cognome nel card body
+
+    let body = document.createElement("h5");
+    body.innerText = name + " " + surname;
+    body.className = "fst-italic";
+    let cardBody = document.createElement("div");
+    cardBody.className= "card-body";
+    cardBody.appendChild(body);
+
+    //creo la sezione card e ci inserisco immagine e card body
+    let cardDiv = document.createElement("div");
+    cardDiv.className = "card col-4";
+    cardDiv.appendChild(image);
+    cardDiv.appendChild(cardBody);
+
+    //aggiungo la sezione card al card holder
+    let holder = document.getElementById("gallery");
+    holder.appendChild(cardDiv);
 }
+
+
+
+}
+
